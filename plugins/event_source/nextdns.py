a7beee7eb633a23851f9dcdcda7533436f5ea274
Colin McNaughton <cloin@users.noreply.github.com>
2023-05-09 10:56:12 -0400

    docs

--- plugins/event_source/nextdns.py
+++ plugins/event_source/nextdns.py
@@ -3,7 +3,12 @@
     nextdns.py
 
 ## Purpose
-    An ansible-rulebook event source plugin for NextDNS logs.
+    An ansible-rulebook event source plugin for NextDNS logs
+
+## Description:
+    Uses the NextDNS API to subscribe to a SSE event stream
+    for the logs of a specific profile and use these as events
+    in Event-Driven Ansible
 
 ## Arguments:
     api_key: Your NextDNS API key
